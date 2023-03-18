5b8d5ca0fbe4f653e67971686c18aca50ec03f38
Sam Chang <sam21323@gmail.com>
2023-03-18 13:10:54 +0800

    Adding login route

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -7,19 +7,21 @@ import Home from './pages/Home/Home';
 import ThankYou from './pages/ThankYou/ThankYou';
 import Product from './pages/Product/Product';
 import Profile from './pages/Profile/Profile';
+import Login from './pages/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path='/' element={<App />}>
         <Route index element={<Home />} />
-        <Route path="products/:id" element={<Product />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="thankyou" element={<ThankYou />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path='products/:id' element={<Product />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='thankyou' element={<ThankYou />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='login' element={<Login />}></Route>
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Route>
     </Routes>
   </BrowserRouter>
