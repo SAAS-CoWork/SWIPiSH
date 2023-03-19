59123e3078f3a934155f9c424e6f5df2349fdb9c
amberliangtesol <amberliangtesol@gmail.com>
2023-03-19 20:43:16 +0800

    fix conflicts again

--- Front-End/src/pages/Login/Login.js
+++ Front-End/src/pages/Login/Login.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import styled from "styled-components";
-import profile from "./profile.png";
-=======
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import profile from './profile.png';
->>>>>>> bfbc045449febe63dd95a2394b20db83863492c4
 
 const questions = [
   { value: 'email', label: '信箱' },
@@ -105,35 +99,6 @@ const LoginBtn = styled.button`
 `;
 
 export default function Login() {
-<<<<<<< HEAD
-  const [userData, setUserData] = useState({});
-
-  function saveUserInput(e) {
-    const key = e.target.name;
-    const userInput = e.target.value;
-    const newUserData = { ...userData, [key]: userInput };
-    setUserData(newUserData);
-  }
-
-  function SendUserData() {
-    const data = {
-      email: userData.email,
-      password: userData.password,
-    };
-    console.log(data);
-
-    fetch("https://www.gotolive.online/api/1.0/user/signin", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm92aWRlciI6Im5hdGl2ZSIsIm5hbWUiOiJTYW0xMjMiLCJlbWFpbCI6InNhbWNoYW4xMjQ1QGdtYWlsLmNvbSIsInBpY3R1cmUiOm51bGwsImlkIjoxMDI5MywiaWF0IjoxNjc5MjEwODcxfQ.PO4gxjBLVlcDq7_vz3InLBqkhX4ve3pf4vTBa55CVjw",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) =>
-        res.status === 200 ? console.log("Success!") : console.log("Error!")
-      )
-=======
   const [userInput, setUserInput] = useState({});
 
   function handleInput(e, data) {
@@ -166,21 +131,16 @@ export default function Login() {
         localStorage.setItem('loginToken', token);
         window.location.href = '/';
       })
->>>>>>> bfbc045449febe63dd95a2394b20db83863492c4
       .catch((err) => console.log(err));
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-<<<<<<< HEAD
-    SendUserData();
-=======
     getJWT(userInput);
   }
 
   function signUp() {
     window.location.href = './register';
->>>>>>> bfbc045449febe63dd95a2394b20db83863492c4
   }
 
   return (
@@ -192,28 +152,6 @@ export default function Login() {
         </LoginInfoTitleContainer>
         <SplitLine></SplitLine>
         <LoginInfoContainer>
-<<<<<<< HEAD
-          <LoginRow>
-            <LoginInfoTitle>帳號</LoginInfoTitle>
-            <LoginInput
-              name="email"
-              value={userData.email}
-              onChange={(e) => saveUserInput(e)}
-            />
-          </LoginRow>
-          <LoginRow>
-            <LoginInfoTitle>密碼</LoginInfoTitle>
-            <LoginInput
-              name="password"
-              value={userData.password}
-              onChange={(e) => saveUserInput(e)}
-            />
-          </LoginRow>
-        </LoginInfoContainer>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <LoginBtn type="submit">登入</LoginBtn>
-        </form>
-=======
           {questions.map((question, index) => (
             <LoginRow key={index}>
               <LoginInfoTitle>{question.label}</LoginInfoTitle>
@@ -231,7 +169,6 @@ export default function Login() {
             成為會員
           </LoginBtn>
         </BtnContainer>
->>>>>>> bfbc045449febe63dd95a2394b20db83863492c4
       </ContentContainer>
     </Wrapper>
   );
