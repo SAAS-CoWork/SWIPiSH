a739a3b7228ae12c3587a6c7f832b673df59414d
SamFang <c.s.fangyolk@gmail.com>
2023-03-20 11:10:15 +0800

    Implement recommendation api

--- Back-End/Stylish-Backend/server/controllers/recommendation_controller.js
+++ Back-End/Stylish-Backend/server/controllers/recommendation_controller.js
@@ -1,20 +1,76 @@
 require('dotenv').config();
-const validator = require('validator');
-const User = require('../models/recommendation_model');
+const Cache = require('../../util/cache');
+const {
+    generateRecommendations,
+    checkIfNewUser,
+    updateLikedProduct
+} = require('../models/recommendation_model');
 
 const swipe = async function (req, res) {
+    const { user } = req;
+    const { product_id } = req.body;
+    const { like } = req.body;
 
+    if ( !product_id || !like ) {
+        return res.status(400).json('Missing required info');
+    }
+
+    const { super_like } = req.body;
+
+    let score = 3;
+    if ( !like ) {
+        score = -1;
+    }
+
+    if ( super_like ) {
+        score = 6;
+    }
+
+    // update liked_product table
+    const successfulUpdate =  await updateLikedProduct( user.id, product_id, score );
+    if ( !successfulUpdate ) {
+        return res.status(500).json('Internal server error');
+    }
+    return res.status(200).json('Updated');
 };
 
 const getRecommendation = async function (req, res) {
     const { user } = req;
 
-    // get recommendation from database
+    try {
+        let isNewUser = false;
+        const checkResult = await checkIfNewUser();
+        if ( checkResult.length === 0 ) {
+            isNewUser = true;
+        }
 
-    // recommendation algorithm
-    
+        if ( isNewUser ) {
+            await generateRecommendations( user.id, true );
+        }
 
+        // get recommendation from database
+        const recommendations = [];
+        for ( let i = 0; i < 10; i++ ) {
+            const reco = await Cache.lPop(user.id);
+            recommendations.push(JSON.parse(reco));
+        }
+        
+        console.log(recommendations);
+        const remained = await Cache.lLen(user.id);
+        if ( remained < 20 ) {
+            await generateRecommendations(user_id, false);
+        }
 
+        const responseObj = {
+            data: recommendations
+        }
+
+        return res.status(200).json(responseObj)
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json('Internal server error');
+    }
 
 };
 
--- Back-End/Stylish-Backend/server/models/recommendation_model.js
+++ Back-End/Stylish-Backend/server/models/recommendation_model.js
@@ -1,17 +1,159 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../../.env'});
 const { pool } = require('./mysqlcon');
-const jwt = require('jsonwebtoken');
+const Cache = require('../../util/cache');
 
-const USER_ROLE = {
-    ALL: -1,
-    ADMIN: 1,
-    USER: 2,
-    VIPUSER: 3
-};
+const findSimilarProducts = async function (user_id) {
+    const [ likedProducts ] = await pool.query(`
+    SELECT score, tags FROM
+    liked_product AS liked_p
+    INNER JOIN
+    product AS p
+    WHERE liked_p.user_id = ? AND liked_p.product_id = p.id;
+    `, [user_id]);
 
+    console.log(likedProducts);
+    const catScores = {};
+    const titleScores = {};
+    const priceScores = {};
+    const textureScores = {};
 
+    likedProducts.map((likedProduct) => {
+        const { score } = likedProduct;
+        const tags = JSON.parse(likedProduct.tags);
+        addScore( catScores, score, tags[0] );
+        addScore( titleScores, score, tags[1] );
+        addScore( priceScores, score, tags[2] );
+        addScore( textureScores, score, tags[3] );
+    })
+
+    const arr = [ catScores, titleScores, priceScores, textureScores ];
+
+    return arr.map((obj) => {
+        return Object.entries(obj).sort((a, b) => {
+            return b[1] - a[1];
+        })
+    });
+}
+
+const generateRecommendations = async function ( user_id, isFirst ) {
+
+    if ( isFirst ) {
+        // generate random 10 recommendations
+        const recommendations = await randomRecommendations();
+        recommendations.forEach((reco) => {
+            Cache.rPush(user_id, JSON.stringify(reco));
+        });
+        return;   
+    }
+
+    const productsScore = await findSimilarProducts(10320);
+    console.log(productsScore);
+
+    const result = findAllSubsets(10320, productsScore);
+    console.log(result);
+    return;
+     
+}
+
+async function randomRecommendations() {
+    const [ products ] = await pool.query(`
+    SELECT id, category, title, story, price, main_image
+    FROM product
+    ORDER BY RAND()
+    LIMIT 20;
+    `);
+    return products;
+}
+
+function addScore( scoresObj, score, tag ) {
+    if ( scoresObj.hasOwnProperty(tag) ) {
+        scoresObj[tag] += score;
+        return;
+    }
+    scoresObj[tag] = score;
+}
+
+async function getPossibleProducts( pattern ) {
+    const [ products ] = await pool.query(`
+    SELECT id, category, title, story, price, main_image
+    FROM product
+    WHERE tags = ?;
+    `, [ pattern ]);
+
+    return products;
+}
+
+function findAllSubsets( user_id, productsScore ) {
+    const result = [];
+    const temp = [];
+    findAllSubsetsHelper( user_id, productsScore, result, temp );
+    return result;
+}
+
+async function findAllSubsetsHelper( user_id, productsScore, result, temp ) {
+    const pos = temp.length;
+    if ( pos === productsScore.length ) {
+        result.push(Array.from(temp));
+        // select matched product and push to redis list
+        const tagsPattern = '[' + String(temp) + ']';
+        const possibleProducts = await getPossibleProducts( tagsPattern );
+        for ( let i = 0; i < possibleProducts.length; i++ ) {
+            Cache.rPush(user_id, JSON.stringify(possibleProducts[i]));
+        }
+    } else {
+        for ( let i = 0; i < productsScore[pos].length; i++ ) {
+            temp.push(productsScore[pos][i][0]);
+            findAllSubsetsHelper( user_id, productsScore, result, temp );
+            temp.pop();
+        }
+    }
+}
+
+async function checkIfNewUser( user_id ) {
+    const [ data ] = await pool.query(`
+    SELECT product_id FROM liked_product WHERE user_id = ?
+    `, [ user_id ]);
+
+    return data;
+}
+
+async function updateLikedProduct( user_id, product_id, score ) {
+
+    try {
+        const conn = await pool.getConnection();
+        await conn.beginTransaction();
+        const [ data ] = await pool.query(`
+        SELECT product_id FROM liked_product WHERE user_id = ? AND product_id = ?
+        `, [ user_id, product_id ]);
+
+        // already liked
+        if ( data.length !== 0 ) {
+            await conn.query(`
+            UPDATE liked_product SET score = score + ? WHERE user_id = ? AND product_id = ?;
+            `, [ score, user_id, product_id ]);
+            await conn.commit();
+            return true;
+        }
+
+        // new like
+        await conn.query(`
+        INSERT INTO liked_product ( user_id, product_id )
+        VALUES ( ?, ? )
+        `, [ user_id, product_id ]);
+        return true;
+
+    } catch (err) {
+        console.error(err);
+        return false;
+    } finally {
+        await conn.release();
+    }
+    
+}
 
 
 module.exports = {
-    USER_ROLE
+    generateRecommendations,
+    checkIfNewUser,
+    updateLikedProduct
 };
