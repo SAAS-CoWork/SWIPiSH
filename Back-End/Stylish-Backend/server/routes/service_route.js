3d2b74c3f431f7ce4f67e08809d344ba095f6d3c
SamFang <c.s.fangyolk@gmail.com>
2023-03-21 16:44:21 +0800

    Implement basic mail api

--- Back-End/Stylish-Backend/server/routes/service_route.js
+++ Back-End/Stylish-Backend/server/routes/service_route.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 
 const { wrapAsync, authentication } = require('../../util/util');
 
-const { sendEmail, getEmailHistory } =  require('../controllers/service_controller');
-
-router.route('/service/email').get(authentication(), wrapAsync(getEmailHistory));
+const { sendEmail } =  require('../controllers/service_controller');
 
 router.route('/service/email').post(authentication(), wrapAsync(sendEmail));
 
