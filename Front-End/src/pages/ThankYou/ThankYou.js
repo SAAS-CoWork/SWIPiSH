c997f2067ad4fc76e6bce35fa75262b7d4cf2672
Sam Chang <sam21323@gmail.com>
2023-03-22 19:45:35 +0800

    modify thankyou page

--- Front-End/src/pages/ThankYou/ThankYou.js
+++ Front-End/src/pages/ThankYou/ThankYou.js
@@ -16,11 +16,13 @@ const Title = styled.div`
   font-weight: bold;
 `;
 
-const Content = styled.div`
-  margin-top: 24px;
-`;
-
 const BackButton = styled.button`
+  border: 1px solid black;
+  background-color: #3f3f3a;
+  height: 50px;
+  width: 150px;
+  border-radius: 8px;
+  color: white;
   margin-top: 24px;
 `;
 
@@ -34,8 +36,6 @@ function ThankYou() {
   return (
     <Wrapper>
       <Title>感謝您的購買，我們會盡快將商品送達！</Title>
-      <Content>請記住以下訂單編號，以便查詢</Content>
-      <Content>{state.orderNumber}</Content>
       <BackButton onClick={() => navigate('/orderStatus')}>查看訂單</BackButton>
     </Wrapper>
   );
